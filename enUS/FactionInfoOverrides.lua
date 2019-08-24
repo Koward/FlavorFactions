424f5c373687ff9a1ea3f4b92ddbce1e609e9fb6
Koward <kiwyh723@gmail.com>
2019-08-24 16:59:04 +0200

    Added databases and fixed Dragonmaw Clan.

--- enUS/FactionInfoOverrides.lua
+++ enUS/FactionInfoOverrides.lua
@@ -3,4 +3,37 @@ if GetLocale() ~= "enUS" then return end
 FactionInfoOverrides = {}
 local L = FactionInfoOverrides
 
+L[16] = {"Riverpaw Gnolls", ""}
+L[17] = {"Redridge Gnolls", ""}
+L[18] = {"Shadowhide Gnolls", ""}
+L[20] = {"Scourge", ""}
+L[26] = {"Bloodscalp Trolls", ""}
+L[27] = {"Skullsplitter Trolls", ""}
+L[33] = {"Frostmane Trolls", ""}
 L[34] = {"Blackrock Clan", ""}
+L[44] = {"Green Dragonflight", ""}
+L[49] = {"Night Watch", ""}
+L[50] = {"Red Dragonflight", ""}
+L[51] = {"Mosshide Gnolls", ""}
+L[52] = {"Dragonmaw Clan", ""}
+L[53] = {"Leper Gnomes", ""}
+L[57] = {"Rothide Gnolls", ""}
+L[78] = {"Mudsnout Gnolls", ""}
+L[80] = {"Black Dragonflight", ""}
+L[82] = {"Witherbark Trolls", ""}
+L[84] = {"Razormane Quillboars", ""}
+L[85] = {"Bristleback Quillboars", ""}
+L[90] = {"Kolkar Clan Centaur", ""}
+L[91] = {"Galak Clan Centaur", ""}
+L[109] = {"Razorfen Quillboars", ""}
+L[110] = {"Razormane Quillboars", ""}
+L[111] = {"Deathshead Quillboars", ""}
+L[531] = {"Bronze Dragonflight", ""}
+L[532] = {"Blue Dragonflight", ""}
+L[572] = {"Vilebranch Trolls", ""}
+
+
+
+
+
+
